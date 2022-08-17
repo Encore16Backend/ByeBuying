e7498163304bf3278b207977e7ff1d92f1683deb
bigboyang <87364146+bigboyang@users.noreply.github.com>
2022-08-17 23:18:38 +0900

    validation

--- src/main/java/com/encore/byebuying/domain/basket/dto/DeleteBasketItemDTO.java
+++ src/main/java/com/encore/byebuying/domain/basket/dto/DeleteBasketItemDTO.java
@@ -4,10 +4,12 @@ import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 public class DeleteBasketItemDTO {
-    @NotBlank
+    @NotNull
+    @Size(min = 1)
     private List<@NotNull Long> basketItemIds;
 }
