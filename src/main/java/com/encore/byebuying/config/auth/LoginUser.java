060c31d7a9e6dd93d990ce92e961eeeff2f4a8c7
qnddjkjh.dev <qnddjkjh.dev@gmail.com>
2022-08-24 17:31:04 +0900

    @AuthenticationPrincipal 로그인 유저 정보 가져오기 설정
     SecurityConfig "/api/users" : 회원가입 전체접근 허용
     OrderController "/api/orders/test" : 로그인 정보 가져오기 test api
     LoginUser : DTO
      - String userId
      - String username
      - RoleType roleType

--- src/main/java/com/encore/byebuying/config/auth/LoginUser.java
+++ src/main/java/com/encore/byebuying/config/auth/LoginUser.java
@@ -7,12 +7,12 @@ import lombok.ToString;
 @Getter
 @ToString
 public class LoginUser {
-    private Long id;
+    private Long userId;
     private String username;
     private RoleType roleType;
 
-    public LoginUser(Long id, String username, RoleType roleType) {
-        this.id = id;
+    public LoginUser(Long userId, String username, RoleType roleType) {
+        this.userId = userId;
         this.username = username;
         this.roleType = roleType;
     }
