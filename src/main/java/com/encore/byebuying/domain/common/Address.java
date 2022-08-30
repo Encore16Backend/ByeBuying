0efde1bf1fb6925e3d2e145a7066f6747135a0d7
hjju <hjju@fin2b.com>
2022-08-30 17:27:30 +0900

    Address 생성자 수정 Location Entity 수정

--- src/main/java/com/encore/byebuying/domain/common/Address.java
+++ src/main/java/com/encore/byebuying/domain/common/Address.java
@@ -1,16 +1,29 @@
 package com.encore.byebuying.domain.common;
 
+import com.encore.byebuying.domain.user.Location;
 import lombok.*;
 
 import javax.persistence.Embeddable;
 
 @Getter
-@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 @Embeddable
 public class Address {
     private String zipcode;
     private String address;
     private String detailAddress;
+
+    // 사용자가 설정해놓거나 새로 생성한 배송지 목록 중에서 선택한 경우
+    public Address(Location location) {
+        this.zipcode = location.getZipcode();
+        this.address = location.getAddress();
+        this.detailAddress = location.getDetailAddress();
+    }
+
+    // 배송지 목록에서 선택한 것이 아닌 주문하면서 새롭게 작성한 경우
+    public Address(String zipcode, String address, String detailAddress) {
+        this.zipcode = zipcode;
+        this.address = address;
+        this.detailAddress = detailAddress;
+    }
 }
--- src/main/java/com/encore/byebuying/domain/user/Location.java
+++ src/main/java/com/encore/byebuying/domain/user/Location.java
@@ -2,21 +2,67 @@ package com.encore.byebuying.domain.user;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
+import java.util.Objects;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Table(name = "user_location_setting")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Location {
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
-	private Long id;
-	private String location;
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  private Long id;
+  @Column(name = "location_name")
+  private String name;
+  @Column(name = "zip_code", length = 191)
+  private String zipcode;
+  @Column(name = "address", length = 1000)
+  private String address;
+  @Column(name = "detail_address", length = 1000)
+  private String detailAddress;
+  @Column(name = "default_location")
+  private boolean defaultLocation;
+  @Column(name = "request_delivery_type", length = 1000)
+  private String requestDeliveryType;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false, referencedColumnName = "user_id")
+  private User user;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Location)) {
+      return false;
+    }
+    Location that = (Location) o;
+    return Objects.equals(getId(), that.getId());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+
+  public Location(String zipcode, String address, String detailAddress) {
+    this.zipcode = zipcode;
+    this.address = address;
+    this.detailAddress = detailAddress;
+  }
 }
