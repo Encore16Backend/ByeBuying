d8ddd87a66bf48a5a6f3eb8cceb5fa258be44eb9
hjju <hjju@fin2b.com>
2022-08-30 16:07:31 +0900

    OrderType에 환불 상태 추가

--- src/main/java/com/encore/byebuying/domain/code/OrderType.java
+++ src/main/java/com/encore/byebuying/domain/code/OrderType.java
@@ -7,9 +7,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum OrderType {
     RECEIPT("주문접수"),    // 주문 접수
-    SHIPPING("배송중"),   // 배송중
-    COMP("배송완료"),       // 배송완료
-    CANCEL("주문취소");      // 주문 취소 (배송중 취소 불가)
+    SHIPPING("배송중"),    // 배송중
+    COMPLETED("배송완료"),       // 배송완료
+    CANCEL("주문취소"),     // 주문 취소 (배송중 취소 불가)
+    REFUNDED("환불");        // 환불
 
     private final String status;
 }
